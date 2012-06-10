29628d7bc7fcf538fb36b5d0ec62b96aebf2ab10
Adi Meyers <adimeyers@gmail.com>
2012-06-10 08:08:20 -0700

    Modfications for Homework 2, Question 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.find(:all, :order => (params[:sort_by]))
+    @sort_column = params[:sort_by]
   end
 
   def new
