07f5388372e32f2caf8e4d50671d678bd2b1d2a8
Adi Meyers <adimeyers@gmail.com>
2012-06-11 07:58:04 -0700

    Cleaned up controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,16 +7,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.find(:all, :order => (params[:sort_by]))
-    if params[:ratings] 
-      @movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
-    end
-    @sort_column = params[:sort_by]
     @all_ratings = Movie.all_ratings
-    @set_ratings = params[:ratings]
-    if !@set_ratings
-      @set_ratings = Hash.new
+    @sort_by = params[:sort_by]
+    @ratings = params[:ratings]
+
+    if @ratings and @sort_by
+      @movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+    elsif @ratings
+      @movies = Movie.where(:rating => params[:ratings].keys)
+    elsif @sort_by
+      @movies = Movie.find(:all, :order => (params[:sort_by]))
+    else
+      @movies = Movie.all
     end
+    if !@ratings
+      @ratings = Hash.new
+    end
+#@movies = Movie.all
+#   if params[:ratings] 
+#@movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+#   end
+#   @sort_column = params[:sort_by]
+#   @all_ratings = Movie.all_ratings
+#   @set_ratings = params[:ratings]
+#   if !@set_ratings
+#     @set_ratings = Hash.new
+#   end
   end
 
   def new
